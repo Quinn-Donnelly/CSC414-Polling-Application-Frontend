61bfd3d149a87c459ddb8d1291d88c93c9b0bd57
Kenneth <kenneth.niles@usm.edu>
2017-11-17 23:28:08 -0600

    Home style

--- app/containers/Home/index.js
+++ app/containers/Home/index.js
@@ -6,17 +6,67 @@
 
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
-import { FormattedMessage } from 'react-intl';
 import { createStructuredSelector } from 'reselect';
+import RaisedButton from 'material-ui/RaisedButton';
+import { Tabs, Tab } from 'material-ui/Tabs';
+import TextField from 'material-ui/TextField';
+import { blue500 } from 'material-ui/styles/colors';
 import makeSelectHome from './selectors';
-import messages from './messages';
 
+const style = {
+  margin: 12,
+};
+
+
+const styles = {
+  headline: {
+    fontSize: 24,
+    paddingTop: 16,
+    marginBottom: 12,
+    fontWeight: 400,
+  },
+  errorStyle: {
+    color: blue500,
+  },
+};
 export class Home extends React.Component { // eslint-disable-line react/prefer-stateless-function
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 'a',
+    };
+  }
+
+  handleChange = (value) => {
+    this.setState({
+      value,
+    });
+  };
+
   render() {
     return (
-      <div>
-        <FormattedMessage {...messages.header} />
-      </div>
+      <Tabs
+        value={this.state.value}
+        onChange={this.handleChange}
+      >
+        <Tab label="Course List" value="a">
+          <div>
+            <h2 style={styles.headline}>Select to go to your course list </h2>
+            <RaisedButton label="Course List" primary style={style} />
+          </div>
+        </Tab>
+        <Tab label="Join" value="b">
+          <div>
+            <h2 style={styles.headline}>Enter in code to join</h2>
+            <TextField
+              hintText="course code"
+              hintStyle={styles.errorStyle}
+            />
+            <RaisedButton label="Join" primary style={style} />
+          </div>
+        </Tab>
+      </Tabs>
     );
   }
 }
