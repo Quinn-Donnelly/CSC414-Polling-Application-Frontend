d25ce38ebe66e240f2318928e5d19a8fbe2b0eb1
Kenneth <kenneth.niles@usm.edu>
2017-11-18 00:14:02 -0600

    Added to button on courselist

--- app/containers/CourseList/index.js
+++ app/containers/CourseList/index.js
@@ -7,20 +7,50 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import Helmet from 'react-helmet';
+import { TextField } from 'material-ui';
 import { createStructuredSelector } from 'reselect';
+import Toggle from 'material-ui/Toggle';
+import RaisedButton from 'material-ui/RaisedButton';
+import Popover from 'material-ui/Popover';
+import Menu from 'material-ui/Menu';
 import { List, ListItem } from 'material-ui/List';
 import ActionInfo from 'material-ui/svg-icons/action/info';
 import Avatar from 'material-ui/Avatar';
 import ActionAssignment from 'material-ui/svg-icons/action/assignment';
-import { blue500, yellow600, orange500, red500, green500, purple500 } from 'material-ui/styles/colors';
-import FloatingActionButton from 'material-ui/FloatingActionButton';
-import ContentAdd from 'material-ui/svg-icons/content/add';
+import { blue500, yellow600 } from 'material-ui/styles/colors';
 import makeSelectCourseList from './selectors';
-const style = {
-  margin: 50,
+
+
+const styles = {
+  errorStyle: {
+    color: blue500,
+  },
 };
 
-export class CourseList extends React.Component { // eslint-disable-line react/prefer-stateless-function
+export class CourseList extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      open: false,
+    };
+  }
+
+  handleTouchTap = (event) => {
+   // This prevents ghost click.
+    event.preventDefault();
+
+    this.setState({
+      open: true,
+      anchorEl: event.currentTarget,
+    });
+  };
+
+  handleRequestClose = () => {
+    this.setState({
+      open: false,
+    });
+  };// eslint-disable-line react/prefer-stateless-function
   render() {
     return (
       <div>
@@ -43,34 +73,31 @@ export class CourseList extends React.Component { // eslint-disable-line react/p
             primaryText="411"
             secondaryText="Jan 20, 2014"
           />
-          <ListItem
-            leftAvatar={<Avatar icon={<ActionAssignment />} backgroundColor={orange500} />}
-            rightIcon={<ActionInfo />}
-            primaryText="306"
-            secondaryText="Jan 20, 2014"
-          />
-          <ListItem
-            leftAvatar={<Avatar icon={<ActionAssignment />} backgroundColor={green500} />}
-            rightIcon={<ActionInfo />}
-            primaryText="410"
-            secondaryText="Jan 20, 2014"
-          />
-          <ListItem
-            leftAvatar={<Avatar icon={<ActionAssignment />} backgroundColor={red500} />}
-            rightIcon={<ActionInfo />}
-            primaryText="203"
-            secondaryText="Jan 20, 2014"
-          />
-          <ListItem
-            leftAvatar={<Avatar icon={<ActionAssignment />} backgroundColor={purple500} />}
-            rightIcon={<ActionInfo />}
-            primaryText="307"
-            secondaryText="Jan 20, 2014"
-          />
-          <FloatingActionButton style={style}>
-            <ContentAdd />
-          </FloatingActionButton>
         </List>
+        <RaisedButton
+          onClick={this.handleTouchTap}
+          primary
+          label="Add Class"
+        />
+
+        <Popover
+          open={this.state.open}
+          anchorEl={this.state.anchorEl}
+          anchorOrigin={{ horizontal: 'left', vertical: 'bottom' }}
+          targetOrigin={{ horizontal: 'left', vertical: 'top' }}
+          onRequestClose={this.handleRequestClose}
+        >
+          <Menu>
+            <TextField
+              hintText=" Course Name"
+              hintStyle={styles.errorStyle}
+            />
+          </Menu>
+          <Toggle
+            label=" Secure"
+            style={styles.toggle}
+          />
+        </Popover>
 
       </div>
     );
